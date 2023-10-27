324686133452a0a94d17e70976102c85e88a05a7
jusnasia21 <nasialu21@gmail.com>
2023-10-27 05:51:36 -0700

    task1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,14 +7,14 @@
   *
   * Return: Always 0.
 */
-void _puts_recursion(char*s)
+void _puts_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
-	}
+	{
 	else
-	_putchar('\n');
+		_putchar('\n');
 }
 
